73b8daf9481cc4a64393a81176a09b5b995b501c
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-19 22:56:00 +0800

    Update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employee extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'role',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function payrollEntries(): HasMany
+    {
+        return $this->hasMany(PayrollEntry::class);
+    }
 }
