13793ad6441d1a45cec97d7999df92a25e85cfd4
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-19 22:45:33 +0800

    Update Customer.php

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'phone',
+        'address',
+        'is_walk_in',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_walk_in' => 'boolean',
+        'is_active' => 'boolean',
+    ];
+
+    public function sales(): HasMany
+    {
+        return $this->hasMany(Sale::class);
+    }
+
+    public function containerMovements(): HasMany
+    {
+        return $this->hasMany(ContainerMovement::class);
+    }
+
+    public function collectionPayments(): HasMany
+    {
+        return $this->hasMany(CollectionPayment::class);
+    }
 }
