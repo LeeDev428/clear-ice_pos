0383e5d3e4a1f5abc138124052849ad66d255155
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-29 22:38:22 +0800

    Update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -10,15 +10,33 @@ class Employee extends Model
     protected $fillable = [
         'name',
         'role',
+        'employee_type',
+        'daily_rate',
+        'ot_rate',
+        'late_rate',
+        'sss_contribution',
+        'philhealth_contribution',
+        'pagibig_contribution',
         'is_active',
     ];
 
     protected $casts = [
         'is_active' => 'boolean',
+        'daily_rate' => 'decimal:2',
+        'ot_rate' => 'decimal:2',
+        'late_rate' => 'decimal:2',
+        'sss_contribution' => 'decimal:2',
+        'philhealth_contribution' => 'decimal:2',
+        'pagibig_contribution' => 'decimal:2',
     ];
 
     public function payrollEntries(): HasMany
     {
         return $this->hasMany(PayrollEntry::class);
+    }
+
+    public function cashAdvances(): HasMany
+    {
+        return $this->hasMany(CashAdvance::class);
     }
 }
